d28e60ba6cfd3ca15feac6293c65aa2f4d93064a
mk2277 <mallikarjun.kaithapuram4@gmail.com>
2022-12-21 15:27:32 -0700

    adding sample files

---
+++ arithmetic/arithmetic.go
@@ -0,0 +1,37 @@
+package arithmetic
+
+import "fmt"
+
+var a, b, sum int
+
+func add() {
+
+	sum = a + b
+	fmt.Println(sum)
+
+}
+
+func sub() {
+
+	sum = a - b
+	fmt.Println(sum)
+
+}
+
+func mul() {
+
+	sum = a * b
+	fmt.Println(sum)
+
+}
+
+func div() {
+
+	sum = a / b
+	fmt.Println(sum)
+
+}
+
+func printing() {
+	fmt.Println("hey hey")
+}
---
+++ impack.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("new program")
+	arithmetic.printing()
+}
